76ceb8b0f50ab4fc5178c181de917dd47f988648
ParaJay <calumjhunter@gmail.com>
2023-03-29 00:03:27 +0100

    removed react-tooltip

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 //import '@/styles/globals.css'
 import "../styles/main.css";
-import 'react-tooltip/dist/react-tooltip.css';
 import Nav from "@/components/Nav";
 
 export default function App({ Component, pageProps }) {
